e22a2b4e10ce2715a8a82a79f0c0dbdd25ad3d20
Fakiri <ismaiilfakirii@gmail.com>
2023-03-21 17:00:12 +0100

    Add New Fields

--- src/items.py
+++ src/items.py
@@ -9,9 +9,19 @@ from scrapy.item import Item, Field
 class ProductItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    _id = Field(deprecated='Product ID')
+    _type = Field(deprecated='Product Type')
     title = Field(deprecated='Product Name')
+    category = Field(deprecated='Category')
     image_url = Field(deprecated='Product Image')
     sponsored = Field(deprecated='Is Sponsored')
     price = Field(deprecated='Price')
+    reduced_price = Field(deprecated='Reduced Price')
+    stock = Field(deprecated='Stock')
+    available = Field(deprecated='Is Available')
+    short_description = Field(deprecated='Short Description')
+    description = Field(deprecated='Description')
+    tax = Field(deprecated='Tax')
     rating_count = Field(deprecated='Rating Count')
     rating_value = Field(deprecated='Rating Value')
+    
